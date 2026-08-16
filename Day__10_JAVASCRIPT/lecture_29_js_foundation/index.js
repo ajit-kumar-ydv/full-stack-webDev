9dcdbe0ddd5a3440173912519763cd6373a2bb2d
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-08-16 10:03:15 +0530

    JAVASCRIPT START

---
+++ Day__10_JAVASCRIPT/lecture_29_js_foundation/index.js
@@ -0,0 +1,34 @@
+console.log("Hello, Ajit");
+
+age = 20;
+name = "ajit";
+console.log(name, age);
+
+
+// var  let  const
+
+// var --- re-declaration and updation both allowed
+/*
+var age; // box created without value
+age = 19; // box me value de di
+var age = 24; // both together declaration and initialization
+var age = 26; // re-declaration 
+console.log(age); // updation
+*/
+
+// let --- only updation allowed
+/*
+let name = "Ajit";
+console.log(name);
+//let name = "Happy"; // error
+console.log(name)
+*/
+
+// const - no updation , no re-declaration
+/*
+const aadhar = 1458679;
+console.log(aadhar);
+const aadhar = 57996448; // error
+aadhar = 699070; // error
+*/
+
